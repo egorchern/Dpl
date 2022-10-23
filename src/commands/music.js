5ec498f6acf5487e3a5647456f3024c4a5145d5a
Egor Chernyshev <egor.chernyshev@student.manchester.ac.uk>
2022-10-23 02:22:47 +0100

    very basic version complete

---
+++ src/commands/music.js
@@ -0,0 +1,74 @@
+const {SlashCommandBuilder} = require("discord.js");
+const Voice = require("@discordjs/voice");
+const youtubedl = require("youtube-dl-exec");
+const got = require("got");
+const download = require("download");
+const fs = require("fs");
+let counter = 0;
+const downloadFile = async (url) => {
+    let filename = `music/music_${counter}.webm`;
+    let tmp = await download(url, "music");
+    return new Promise((resolve) => {
+        fs.writeFile(filename, tmp, (err) => {
+            resolve({filename, err});
+        });
+    });
+};
+const fetchYoutubeAudioUrl = async (url) => {
+    let info = await youtubedl(url, {
+        dumpSingleJson: true,
+        noCheckCertificates: true,
+        noWarnings: true,
+        preferFreeFormats: true,
+        addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+        format: "bestaudio",
+    });
+    let full_url = info.requested_downloads[0].url;
+    return full_url;
+};
+
+FFMPEG_OPTIONS = {
+    before_options: "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+    options: "-vn",
+};
+
+const join_channel = async (message) => {
+    const voiceChannel = message.member.voice.channel;
+    // Member not in a voice channel
+    if (!voiceChannel) {
+        return false;
+    }
+    const connection = Voice.joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: voiceChannel.guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        selfDeaf: false,
+        selfMute: false,
+    });
+    return connection;
+};
+//https://www.youtube.com/watch?v=fhXE7hhNPmM
+
+const playYoutubeAudio = async (message, youtube_url) => {
+    const channel_info = await join_channel(message);
+    if (channel_info) {
+        if (!youtube_url) return;
+        let url = await fetchYoutubeAudioUrl(youtube_url);
+        // let file_info = await downloadFile(url)
+        // const stream = got.stream(url);
+        const stream = url;
+        // const stream = fs.createReadStream(file_info.filename)
+        const player = Voice.createAudioPlayer();
+        const resource = Voice.createAudioResource(stream, {
+            inputType: Voice.StreamType.WebmOpus,
+        });
+        player.play(resource);
+        channel_info.subscribe(player);
+    }
+    return
+};
+
+module.exports = {
+    playYoutubeAudio,
+    fetchYoutubeAudioUrl,
+};
---
+++ src/index.js
@@ -0,0 +1,85 @@
+const Discord = require("discord.js");
+const intentsEnum = Discord.GatewayIntentBits;
+const Events = Discord.Events;
+const config = require("../config.json");
+const path = require("path");
+const fs = require("fs");
+const Music = require("./commands/music");
+const command_prefix = "!";
+
+const client = new Discord.Client({
+    intents: [
+        intentsEnum.Guilds,
+        intentsEnum.GuildMessages,
+        intentsEnum.MessageContent,
+        intentsEnum.GuildVoiceStates,
+    ],
+});
+
+client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// const commandsPath = path.join(__dirname, "commands");
+// const commandFiles = fs
+//     .readdirSync(commandsPath)
+//     .filter((file) => file.endsWith(".js"));
+// client.commands = new Discord.Collection();
+
+// for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     const command = require(filePath);
+//     // Set a new item in the Collection with the key as the command name and the value as the exported module
+//     if ("data" in command && "execute" in command) {
+//         client.commands.set(command.data.name, command);
+//     } else {
+//         console.log(
+//             `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+//         );
+//     }
+// }
+// client.on(Events.InteractionCreate, async (interaction) => {
+//     if (!interaction.isChatInputCommand()) return;
+//     const command = interaction.client.commands.get(interaction.commandName);
+
+//     if (!command) {
+//         console.error(
+//             `No command matching ${interaction.commandName} was found.`
+//         );
+//         return;
+//     }
+
+//     try {
+//         await command.execute(interaction);
+//     } catch (error) {
+//         console.error(error);
+//         await interaction.reply({
+//             content: "There was an error while executing this command!",
+//             ephemeral: true,
+//         });
+//     }
+// });
+const sendReply = async (messageObj, text) => {
+    messageObj.channel.send({
+        content: text,
+        reply: messageObj
+    });
+    
+    console.log("sent");
+};
+
+client.on("messageCreate", async (message) => {
+    if (!message.content.startsWith(command_prefix)) return;
+    let localMsg = message.content.slice(1, message.content.length);
+    let parts = localMsg.split(" ");
+    switch (parts[0]) {
+        case "play": {
+            try {
+                let youtube_url = parts[1];
+                Music.playYoutubeAudio(message, youtube_url);
+                sendReply(message, `Playing ${youtube_url}`)
+            } catch {}
+        }
+    }
+});
+client.login(process.env.BOT_TOKEN);
